7e3bee9c9a99ab6006c9b519b2713ac34db36973
Akash Soni <149365051+SoniAkash9140@users.noreply.github.com>
2024-04-03 09:23:57 +0530

    Add files via upload
    Nested If Else

--- percentage.c
+++ percentage.c
@@ -1,19 +1,29 @@
 #include<stdio.h>
 int main(){
+    int a;
+    printf("Enter the number from 0 to 100:");
+    scanf("%d",&a);
 
-    float x1=85;
-
-    float x2=80;
-
-    float x3=90;
-
-    float x4=85;
-
-    float x5=90;
-
-    float percent=(x1+x2+x3+x4+x5)/5;
-
-    printf("%f",percent);
-
+    if(91<a<100){
+        printf("Excellent");
+    }
+        else if(81<a<90){
+            printf("Very Good");
+        }
+            else if(71<a<80){
+                printf("good");
+            }
+                else if(61<a<70){
+                    printf("Can be better");
+                }
+                   else if(51<a<60){
+                        printf("Average");
+                   }
+                        else if(41<a<50){
+                            printf("below average");
+                        }
+                            else{
+                                printf("Fail");
+                            }
     return 0;
 }

12653d6fde13d56d40f062e5d355c615be8548fc
Akash Soni <149365051+SoniAkash9140@users.noreply.github.com>
2024-04-03 09:22:28 +0530

    if else
    If Else

---
+++ leapyear.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int n; 
+    printf("Enter a year:");
+    scanf("%d",&n);
+    if(n%4==0){
+        printf("Leap Year");
+    }
+    else{
+        printf("Not a Leap Year");
+    }
+
+    return 0;
+}   

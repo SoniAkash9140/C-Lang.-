b9794fc64509315f7acd954e60f4f28d675f225c
Akash Soni <149365051+SoniAkash9140@users.noreply.github.com>
2024-06-02 15:49:15 +0530

    Update leapyear.c

--- leapyear.c
+++ leapyear.c
@@ -1,14 +1,20 @@
-#include<stdio.h>
-int main(){
-    int n; 
-    printf("Enter a year:");
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int n ; 
+    printf("Enter the year");
     scanf("%d",&n);
-    if(n%4==0){
-        printf("Leap Year");
-    }
-    else{
-        printf("Not a Leap Year");
+    
+    if(n%400==0){
+        printf("Leap Year");}
+        else if(n%100==0){
+            printf("Not a leap year");}
+            else if(n%4==0){
+                printf("Leap Year");}
+                else{
+                    printf("Not a leap year");
     }
 
     return 0;
-}   
+}
